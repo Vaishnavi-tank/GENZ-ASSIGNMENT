5e25f897bc0e054a84003b4edcda675ec5919077
Vaishnavi-tank <vaishnavitank95@gmail.com>
2025-05-14 13:22:05 +0530

    Update Resultsection.jsx

--- FRONTEND/src/component/Resultsection.jsx
+++ FRONTEND/src/component/Resultsection.jsx
@@ -44,12 +44,12 @@ const Resultsection = () => {
         </div>
       </div>
       */}
-     <div className="banner5 w-full max-w-[750px] mx-auto px-4 py-10">
+     <div className=" w-full max-w-[750px] mx-auto px-4 py-10">
   <h1 className="text-3xl md:text-4xl text-center text-yellow-800 font-bold mb-8">
     What GenZ Happy Clients Say
   </h1>
 
-  <div className="backdrop-blur-md bg-white/10 rounded-xl p-4">
+  <div className="backdrop-blur-md bg-black/40 rounded-xl p-4">
     <Slider
       dots={true}
       infinite={true}
@@ -84,6 +84,62 @@ const Resultsection = () => {
           ❝ Loved the services and accuracy of the work with zero delays. Everything was delivered on time and according to my requirements. ❞
         </p>
         <h2 className="font-semibold text-2xl">Andrea Henry - UK</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 4 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ Got to know them through my colleague, and I'm really thankful to him for suggesting this platform. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">David Smith - Germany</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 5 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ The work is deliver on-time and according to my requirements. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">Robert Nicholson - Germany</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 6 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ The kind of work they provide is exemplary. It is like a stop solution to all our problems. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">Alisha Fernandez - UK</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 7 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ The support team helps you out even if you have a small query. It's the highlight of the company. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">Sophie Calley - New Zealand</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 8 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ Loved the services and accuracy of work with zero delays. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">Kay Lynn - UK</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 9 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ The job being done with latest technologies which make my website look more attractive.❞
+        </p>
+        <h2 className="font-semibold text-2xl">Wendy Allis - USA</h2>
+        <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
+      </div>
+      {/* Slide 10 */}
+      <div className="text-black text-center px-4 py-6 font-bold">
+        <p className="text-xl  md:text-xl leading-relaxed mb-4 italic">
+          ❝ Received my work within given time period, and the work was also upto the mark. ❞
+        </p>
+        <h2 className="font-semibold text-2xl">Sam Oakes - UAE</h2>
         <p className="text-yellow-400">⭐⭐⭐⭐⭐</p>
       </div>
     </Slider>

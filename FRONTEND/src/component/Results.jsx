c2b0e8e39642787b3d7c45cea8aff4ad23ffe1df
Vaishnavi-tank <vaishnavitank95@gmail.com>
2025-05-14 13:01:52 +0530

    Update Results.jsx

--- FRONTEND/src/component/Results.jsx
+++ FRONTEND/src/component/Results.jsx
@@ -15,7 +15,7 @@ const Results = () => {
         useEffect(() => {
           const timer = setTimeout(() => {
             setIsOpen(true);
-          }, 3000);
+          }, 20000);
       
           return () => clearTimeout(timer);
         }, []);

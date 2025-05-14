ecdcd2f8d1475ae02b2e2820e6a2924647730258
Vaishnavi-tank <vaishnavitank95@gmail.com>
2025-05-14 12:58:07 +0530

    Update Contact.jsx

--- FRONTEND/src/component/Contact.jsx
+++ FRONTEND/src/component/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         useEffect(() => {
           const timer = setTimeout(() => {
             setIsOpen(true);
-          }, 3000);
+          }, 20000);
       
           return () => clearTimeout(timer);
         }, []);
